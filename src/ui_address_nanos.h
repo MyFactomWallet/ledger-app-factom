ee2b44efb2e02676c784007a7712fd49a2f088de
Dennis Bunfield <dennis@factoid.org>
2020-01-27 17:34:33 -0600

    cleanup and expanded the number of JSON available for FAT

--- src/ui_address_nanos.h
+++ src/ui_address_nanos.h
@@ -66,7 +66,7 @@ const bagl_element_t ui_address_nanos[] = {
      NULL},
     {{BAGL_LABELINE, 0x02, 23, 26, 82, 12, 0x80 | 10, 0, 0, 0xFFFFFF, 0x000000,
       BAGL_FONT_OPEN_SANS_EXTRABOLD_11px | BAGL_FONT_ALIGNMENT_CENTER, 26},
-     (char *)fullAddress,
+     (char *)outstr.fullAddress,
      0,
      0,
      0,
