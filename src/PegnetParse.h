69a81ce3e7c6561a218e6ea574b05b112afa2be7
Dennis Bunfield <dennis@factoid.org>
2020-04-30 17:16:50 -0500

    src/PegnetParse.c

--- src/PegnetParse.h
+++ src/PegnetParse.h
@@ -1,13 +1,12 @@
-#ifndef _FAT_PARSE_H_
-#define _FAT_PARSE_H_
+#ifndef _PEGNET_PARSE_H_
+#define _PEGNET_PARSE_H_
 
 #include "fctParse.h"
 #include "fatParse.h"
 
-//int toString(char a[], int len);
 
-int processPegTx( char *inputaddress, int8_t *data, uint32_t length,
-                       txContent_t *content) ;
+int processPegTx(const char *inputaddress, int r, jsmntok_t *t, int8_t *d, uint32_t length, txContent_t *content);
+
 
 #endif
 
